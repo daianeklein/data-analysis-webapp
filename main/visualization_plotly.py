37db4fc62aa1243d87ef5293e591058f881bd683
Daiane Klein <daiane.klein22@gmail.com>
2022-10-29 18:06:49 -0300

    added cluster viz

--- main/visualization_plotly.py
+++ main/visualization_plotly.py
@@ -47,8 +47,9 @@ def plot_final_visualization(dataframe1, dataframe2):
 
     fig.add_trace(go.Histogram(x=dataframe1['total_score']), row = 1, col= 1)
     fig.add_trace(go.Histogram(x=dataframe2['total_score']), row = 1, col = 1)
-
-
+    fig.add_trace(go.Scatter(x = dataframe1['pricepoint_sum_jitter'], y = dataframe1['total_score_jitter'],
+        mode = 'markers', marker_color=dataframe1['cluster']), row = 1, col = 2)
+    fig.update_coloraxes(showscale=False)
 
     #overlay both histograms
     fig.update_layout(barmode='overlay')
@@ -56,6 +57,7 @@ def plot_final_visualization(dataframe1, dataframe2):
 
     fig.show()
 
-plot_final_visualization(delivered, failed)
+
+plot_final_visualization(failed,delivered)
 
 
